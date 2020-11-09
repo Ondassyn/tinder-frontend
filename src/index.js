5ea3ebbc0479a4122281a7522b95a619c76a0225
Ondassyn <oabdrakhmanov@nu.edu.kz>
2020-11-09 18:50:59 +0600

    changed index.html

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '../build/index.html';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
